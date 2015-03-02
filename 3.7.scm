02e5063eed31ba55c90392d65c0a2fdcade1ffd7
takkyuuplayer <takkyuuplayer@gmail.com>
2015-03-02 10:48:36 +0000

    Add 3.7

---
+++ 3.7.scm
@@ -0,0 +1,38 @@
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch p m)
+    (cond ((not (eq? p password)) (lambda (args...) "Incorrect password"))
+          ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          ((eq? m 'identify) (eq? pwd password))
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+  dispatch)
+
+; 3.7
+(define (make-joint account orig-pass password)
+  (if (account 'origpass 'identify)
+    (lambda (p m)
+      (cond ((not (eq? p password)) (lambda (args...) "Incorrect password"))
+            (else (account orig-pass m))
+            ))
+    (print "Invalid password")
+    ))
+
+
+(define acc (make-account 100 'secret-password))
+(print ((acc 'secret-password 'withdraw) 10))
+
+(define copy-acc (make-joint acc 'secret-password 'my-password))
+(print ((copy-acc 'secret-password 'withdraw) 10))
+(print ((copy-acc 'my-password 'withdraw) 10))
+
+(define invalid-acc (make-joint acc 'invalid-password 'my-password))
+(print ((invalid-acc 'my-password 'withdraw) 10))
