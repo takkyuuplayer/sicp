3df20c3d64ad964fdeffa34c88b3ee756f2286ec
takkyuuplayer <takkyuuplayer@gmail.com>
2015-09-11 12:44:04 +0900

    Fix 3.48

--- 3.48.scm
+++ 3.48.scm
@@ -1,3 +1,37 @@
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          (mutex 'release)
+          val)) serialized-p)))
+
+
+(define (make-mutex)
+  (define (test-and-set! cell)
+    (if (car cell)
+      true
+      (begin (set-car! cell #t)
+             #f)))
+  (define (clear! cell)
+      (set-car! cell #f))
+  (let ((cell (list #f)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+               (the-mutex 'acquire))) ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+; 3.48
+
 (define (auto-incrementor)
   (let ((id 0))
     (lambda ()
@@ -9,7 +43,7 @@
     )
   )
 
-(define issue-account-id (incrementor))
+(define issue-account-id (auto-incrementor))
 
 (define (make-account-and-serializer balance)
   (define (withdraw amount)
@@ -27,7 +61,7 @@
             ((eq? m 'deposit) deposit)
             ((eq? m 'balance) balance)
             ((eq? m 'serializer) balance-serializer)
-            ((eq? m 'account-id) (lambda () account-id))
+            ((eq? m 'account-id) account-id)
             (else (error "Unknown request -- MAKE-ACCOUNT"
                          m))))
     dispatch))
@@ -35,9 +69,23 @@
 (define (serialized-exchange account1 account2)
   (let ((serializer1 (account1 'serializer))
         (serializer2 (account2 'serializer)))
-    (if (> (account1 'account-id) (account2 'account-id))
-      (serializer1 (serializer2 exchange))
-      (serializer2 (serializer1 exchange))
+    (
+        (if (> (account1 'account-id) (account2 'account-id))
+            (serializer1 (serializer2 exchange))
+            (serializer2 (serializer1 exchange))
+        )
      account1
      account2)))
+
+
+; Test
+
+(define account1 (make-account-and-serializer 100))
+(define account2 (make-account-and-serializer 200))
+(print (account1 'balance))
+(print (account2 'balance))
+
+(serialized-exchange account1 account2)
+(print (account1 'balance))
+(print (account2 'balance))
 
