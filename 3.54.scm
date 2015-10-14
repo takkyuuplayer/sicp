92c78747567c61f97d8f5e06a7689a58d0a2721e
takkyuuplayer <takkyuuplayer@gmail.com>
2015-10-14 19:32:52 +0900

    Fix number

--- 3.54.scm
+++ 3.54.scm
@@ -49,13 +49,15 @@
     (stream-map + s1 s2))
 
 (define ones (cons-stream 1 ones))
-(define integers (cons-stream 1 (add-streams ones integers)))
+(define (integers-starting-from n)
+    (cons-stream n (integers-starting-from (+ n 1))))
+
 
 ; 3.54 mul-streams
 (define (mul-streams s1 s2)
     (stream-map * s1 s2))
 
-(define factorials (cons-stream 1 (mul-streams integers factorials)))
+(define factorials (cons-stream 1 (mul-streams (integers-starting-from 2) factorials)))
 (print (stream-ref factorials 0))
 (print (stream-ref factorials 1))
 (print (stream-ref factorials 2))
