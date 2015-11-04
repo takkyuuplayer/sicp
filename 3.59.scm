79e63fef89ed4253ea8f471a8507dff13bce995b
takkyuuplayer <takkyuuplayer@gmail.com>
2015-11-04 21:04:13 +0900

    Add 3.59 b

--- 3.59.scm
+++ 3.59.scm
@@ -66,3 +66,15 @@
 ; 3.59 a test
 (define integrated-all-one-series (integrate-stream ones))
 (stream-head integrated-all-one-series 5) ; 1, 1/2, 1/3, 1/4
+(print "")
+
+; 3.59 b
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+(define minus-ones (cons-stream -1 minus-ones))
+(define cosine-series (cons-stream 1 (mul-streams minus-ones (integrate-stream sine-series))))
+(define sine-series (cons-stream 0 (integrate-stream cosine-series)))
+
+(stream-head sine-series 5)
+(print "")
+(stream-head cosine-series 5)
