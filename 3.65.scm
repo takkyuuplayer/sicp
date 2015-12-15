12d9807c8a0bf1ea915b106cc6d33aa3bee3fc1b
takkyuuplayer <takkyuuplayer@gmail.com>
2015-12-15 12:35:33 +0900

    Add 3.65

---
+++ 3.65.scm
@@ -0,0 +1,18 @@
+(load "./lib/stream/base.scm")
+(load "./lib/stream/list.scm")
+(load "./lib/stream/calc.scm")
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))           ; Sn-1
+        (s1 (stream-ref s 1))           ; Sn
+        (s2 (stream-ref s 2)))          ; Sn+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+; 3.65
+(define (ln2-summand n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (ln2-summands (+ n 1)))))
+(define ln2-stream
+  (partial-sums (ln2-summands 1)))
