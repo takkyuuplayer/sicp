1e396928e6f9d69070a50a0bbbba9b8ffce48b5a
takkyuuplayer <takkyuuplayer@gmail.com>
2016-01-08 12:30:54 +0900

    Add 3.70

--- 3.70.scm
+++ 3.70.scm
@@ -7,27 +7,48 @@
 (define integers (cons-stream 1 (add-streams ones integers)))
 
 ; 3.70
-(define (merge-weighted s1 s2 weight)
-  (cond ((stream-null? s1) s2)
-        ((stream-null? s2) s1)
+(define (merge-weighted pairs1 pairs2 weight)
+  (cond ((stream-null? (stream-car pairs1)) pairs2)
+        ((stream-null? (stream-car pairs2)) pairs1)
         (else
-          (let ((s1car (stream-car s1))
-                (s2car (stream-car s2)))
-            (cond ((< (weight s1car) (weight s2car))
-                   (cons-stream s1car (merge (stream-cdr s1) s2)))
-                  ((> (weight s1car) (weight s2car))
-                   (cons-stream s2car (merge s1 (stream-cdr s2))))
-                  (else
-                    (cons-stream s1car
-                                 (merge (stream-cdr s1)
-                                        (stream-cdr s2)))))))))
+          (let ((p1car (stream-car pairs1))
+                (p2car (stream-car pairs2)))
+            (if (< (weight p1car) (weight p2car))
+              (cons-stream p1car (merge-weighted pairs2 (stream-cdr pairs1) weight))
+              (cons-stream p2car (merge-weighted pairs1 (stream-cdr pairs2) weight)))))))
+
+(define (weighted-pairs s t weight)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (merge-weighted
+      (stream-map (lambda (x) (list (stream-car s) x))
+                  (stream-cdr t))
+      (weighted-pairs (stream-cdr s) (stream-cdr t) weight)
+      weight)))
+
 
 ; 3.70 a
-(define (weighted-pairs s1 s2 weight)
-  ...
-  )
+(define (add-pairs-weight pair)
+  (+ (car pair) (cadr pair)))
+
+(define p (weighted-pairs integers integers add-pairs-weight))
+(stream-head p 10)
+
 
 ; 3.70 b
-(define (weighted-pairs s1 s2 weight)
-  ...
-  )
+(define (add-pairs-weight2 pair)
+  (let ((i (car pair))
+        (j (cadr pair)))
+    (+ (* 2 i)
+       (* 3 j)
+       (* 5 i j))))
+
+(define integers-no-remainder-2-3-5
+  (stream-filter (lambda (x)
+                   (not (or (= 0 (remainder x 2))
+                             (= 0 (remainder x 3))
+                             (= 0 (remainder x 5)))))
+                 integers))
+
+(define q (weighted-pairs integers-no-remainder-2-3-5 integers-no-remainder-2-3-5 add-pairs-weight2))
+(stream-head q 10)
