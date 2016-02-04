395575d9dc2043ffd93bac846fc2ff3fe83bab6d
takkyuuplayer <takkyuuplayer@gmail.com>
2016-02-04 12:58:28 +0900

    Add 3.81

---
+++ 3.81.scm
@@ -0,0 +1,37 @@
+(load "./lib/stream/base.scm")
+(load "./lib/stream/list.scm")
+(load "./lib/stream/calc.scm")
+(load "./lib/stream/display.scm")
+
+(define (rand-update x)
+  (let ((a (expt 2 32))
+        (c 1103515245)
+        (m 12345))
+    (modulo (+ (* a x) c) m)))
+(define random-init 137)
+
+; 3.81
+(define (random-numbers stream)     ; e.g. stream = ('generate 'generate 123 256 'generate .....)
+  (let ((m (stream-car stream)))
+    (define (next m x)
+      (if (eq? m 'generate) (rand-update x) m)
+      )
+    (if (eq? m 'generate)
+      (cons-stream random-init (stream-map next stream (random-numbers stream)))
+      (cons-stream m (stream-map next (stream-cdr stream) (random-numbers stream)))
+      )
+    )
+  )
+
+; test 
+(define s0 (cons-stream 'generate s0))
+(define s1
+  (cons-stream
+    367
+    (cons-stream
+      'generate (cons-stream random-init s0))))
+
+(define rs0 (random-numbers s0))
+(define rs1 (random-numbers s1))
+(stream-head rs0 10)
+(stream-head rs1 10)
