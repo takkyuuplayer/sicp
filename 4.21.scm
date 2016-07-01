40258b5b8a6c7ad5c55a79704064582c4a9d88f5
takkyuuplayer <takkyuuplayer@gmail.com>
2016-07-01 12:31:44 +0900

    Add 4.21 b

--- 4.21.scm
+++ 4.21.scm
@@ -8,3 +8,17 @@
         (* k (ft ft (- k 1)))))))
  10)
 ) ; 3628800
+
+; b
+
+(define (f x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) #t (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) #f (ev? ev? od? (- n 1))))))
+
+
+(print (f 4)) ; #t
+(print (f 5)) ; #f
